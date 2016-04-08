f66b1351f5d0d942f4badf585e57eaa021b4296c
ali7ussein <ali7ussein@yahoo.com>
2016-04-08 09:54:56 +0200

    Side bar Style Changes

--- public/app/main/mvSidebarCtrl.js
+++ public/app/main/mvSidebarCtrl.js
@@ -1,4 +1,25 @@
 angular.module('app').controller('mvSidebarCtrl',
-    function ($scope, $http, $location, mvIdentity) {
+    function ($scope, $http, $location, mvIdentity, mvNotifier, mvAuth,$translate,$localStorage) {
         $scope.identity = mvIdentity;
+
+        $scope.signin = function (username, password,rememberme) {
+            mvAuth.authenticatedUser(username, password,rememberme).then(function (success) {
+                if (success) {
+                    mvNotifier.notify('You have successfully signed in');
+                } else {
+                    mvNotifier.error('Username/Password combination incorrect');
+                }
+            });
+        };
+
+        $scope.signout = function () {
+            mvAuth.logoutUser().then(function () {
+                $scope.username = '';
+                $scope.password = '';
+                mvNotifier.notify('You have successfully signed out');
+                $location.path('/');
+            });
+        };
+
+
     });
