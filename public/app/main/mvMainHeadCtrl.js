9bdd9701d45e80abb33cacbb638c92ba1402e3da
ali7ussein <ali7ussein@yahoo.com>
2016-04-11 21:35:56 +0200

    RTL

---
+++ public/app/main/mvMainHeadCtrl.js
@@ -0,0 +1,15 @@
+﻿angular.module('app').controller('mvMainHeadCtrl', function ($scope , $rootScope) {
+    $scope.styleFile = "vendor/bootstrap/dist/css/bootstrap.min.css";
+    $scope.rtlstyleFile = "vendor/bootstrap/dist/css/bootstrap-rtl.css";
+
+    $scope.getStyleFile = function () {
+        console.log($rootScope.currentLang);
+        if($rootScope.currentLang == 'ar')
+            return "vendor/bootstrap/dist/css/bootstrap-rtl.css";
+        else if ($rootScope.currentLang == 'en')
+            return "vendor/bootstrap/dist/css/bootstrap.min.css";
+    };
+
+
+});
+
