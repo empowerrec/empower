527721c0f0fac15c119336729b2ea3adbbcabe75
ali7ussein <ali7ussein@yahoo.com>
2016-04-12 08:56:58 +0200

    RTL Inhancments

---
+++ public/app/main/mvLogoCtrl.js
@@ -0,0 +1,6 @@
+angular.module('app').controller('mvLogoCtrl', function ($scope) {
+       
+    $scope.title = "Empower";
+    $scope.subTitle = "The Hand That Help";
+
+});
--- public/app/main/mvMainHeadCtrl.js
+++ public/app/main/mvMainHeadCtrl.js
@@ -1,7 +1,7 @@
 ﻿angular.module('app').controller('mvMainHeadCtrl', function ($scope , $rootScope) {
     $scope.styleFile = "vendor/bootstrap/dist/css/bootstrap.min.css";
     $scope.rtlstyleFile = "vendor/bootstrap/dist/css/bootstrap-rtl.css";
-
+    
     $scope.getStyleFile = function () {
         console.log($rootScope.currentLang);
         if($rootScope.currentLang == 'ar')
