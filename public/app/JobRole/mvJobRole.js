347725a1c7e2198e934f9007d0b018d8f6d9d395
ali7ussein <ali7ussein@yahoo.com>
2016-10-03 15:23:47 +0200

    delete files

--- public/app/JobRole/mvJobRole.js
+++
@@ -1,6 +0,0 @@
-angular.module('app').factory('mvJobRole', function ($resource) {
-    var jobRoleResource = $resource('/api/jobRoles/:_id', {_id: '@id'},
-        {update: {method: 'PUT', isArray: false}
-    });
-    return jobRoleResource;
-});
--- public/app/JobRole/mvJobRoleDetailCtrl.js
+++
@@ -1,3 +0,0 @@
-angular.module('app').controller('mvJobRoleDetailCtrl', function ($scope, mvJobRole, $routeParams) {
-    $scope.jobRole = mvJobRole.get({_id: $routeParams.id});
-});
