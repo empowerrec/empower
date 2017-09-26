5e10f3c9ddae191ce2d2e435868b0ae039302dd1
ali7ussein <ali7ussein@yahoo.com>
2017-09-26 09:11:49 +0200

    travel pre

--- server/models/JobSeeker.js
+++ server/models/JobSeeker.js
@@ -325,10 +325,10 @@ var jobSeekerSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'JobRole'
     }],
-    TravelPreference: [{
+    TravelPreference: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'TravelPreference'
-    }],
+    },
     ReferenceFullName: { type: String },
     ReferencePosition: { type: String },
     ReferenceCompany: { type: String },
